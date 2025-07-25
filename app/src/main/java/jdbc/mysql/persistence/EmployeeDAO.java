eb787306d3bcf34ebf7f789be97b8880d1383b79
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-07-25 15:16:52 -0300

    feat: EmployeeDAO findAll()

--- app/src/main/java/jdbc/mysql/persistence/EmployeeDAO.java
+++ app/src/main/java/jdbc/mysql/persistence/EmployeeDAO.java
@@ -1,31 +1,36 @@
 package jdbc.mysql.persistence;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.OffsetDateTime;
+import static java.time.ZoneOffset.UTC; 
+import java.util.ArrayList;
 import java.util.List;
 
 import jdbc.mysql.persistence.entity.EmployeeEntity;
 
 public class EmployeeDAO {
-    public static void insert(final EmployeeEntity entity){
+    public static void insert(final EmployeeEntity entity) {
 
         String sql = "INSERT INTO employees (name, salary, birthday) values ( ?, ?, ?)";
-        try(
-            var connection = ConnectionUtil.getConnection();
-            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-                
+        try (
+                var connection = ConnectionUtil.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
             statement.setString(1, entity.getName());
             statement.setBigDecimal(2, entity.getSalary());
             statement.setObject(3, entity.getBirthday());
 
             int affectedRows = statement.executeUpdate();
             System.out.printf("Affected %d rows%n", affectedRows);
-            
-            try(var generatedKeys = statement.getGeneratedKeys()){
-                if(generatedKeys.next()) entity.setId(generatedKeys.getLong(1));
+
+            try (var generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next())
+                    entity.setId(generatedKeys.getLong(1));
             }
-            
+
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
@@ -39,8 +44,31 @@ public class EmployeeDAO {
 
     }
 
-    public List<EmployeeEntity> findAll() {
-        return null;
+    public static List<EmployeeEntity> findAll() {
+        List<EmployeeEntity> allEmployees = new ArrayList<>();
+        String sql = "SELECT * FROM employees ORDER BY name ASC";
+
+        try (
+            var connection = ConnectionUtil.getConnection();
+            Statement statement = connection.createStatement()) {
+            
+            statement.executeQuery(sql);
+            ResultSet resultSet = statement.getResultSet();
+
+            while(resultSet.next()){
+                EmployeeEntity employee = new EmployeeEntity();
+                employee.setId(resultSet.getLong("id"));
+                employee.setName(resultSet.getString("name"));
+                employee.setSalary(resultSet.getBigDecimal("salary"));
+                var birthdayInstant = resultSet.getTimestamp("birthday").toInstant();
+                employee.setBirthday(OffsetDateTime.ofInstant(birthdayInstant, UTC));
+                allEmployees.add(employee);
+            }
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return allEmployees;
     }
 
     public EmployeeEntity findById() {
